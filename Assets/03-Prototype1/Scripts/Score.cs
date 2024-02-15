42cfe5df68360de9fababf8e6c73b9c28a5ef9b8
JackTesoro <jt1107@scarletmail.rutgers.edu>
2024-02-15 15:08:04 -0500

    Prototype high score
    added a highscore UI

--- Assets/03-Prototype1/Scripts/Score.cs
+++ Assets/03-Prototype1/Scripts/Score.cs
@@ -8,8 +8,10 @@ public class Score : MonoBehaviour
     public static Score instance;
 
     public Text scoreText;
+    public Text highscoreText;
 
     int score = 0;
+    int highscore = 0;
 
     private void Awake()
     {
@@ -18,11 +20,15 @@ public class Score : MonoBehaviour
     // Update is called once per frame
     void Start()
     {
+        highscore = PlayerPrefs.GetInt("highscore", 0);
         scoreText.text = "Score: " + score.ToString();
+        highscoreText.text = "Highscore: " + highscore.ToString();
     }
     public void AddPoint()
     {
         score += 1;
         scoreText.text = "Score: " + score.ToString();
+        if (highscore < score)
+            PlayerPrefs.SetInt("highscore", score);
     }
 }
